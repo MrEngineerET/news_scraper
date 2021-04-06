cd1c98f77d93e0eaf3c466e5aae5753dd3730f5a
Biruk <meetbirukberhanu@gmail.com>
2021-04-06 08:52:32 +0300

    scraped file is now to the database, code is modified to run from the server index.js

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 const fs = require('fs')
-const spinner = require('ora')
+const path = require('path')
+const debugScraper = require('debug')('debugScraper')
 
+const { createNews } = require('./Service/newsServices')
 const { loader } = require('./src/loader')
 const { parser } = require('./src/parser')
 
-// reading the base configration file which contain path and to the news configration file
-spinner('Started Reading config.json file').start()
-let configs = readJSONfile('configs/configs.json')
-spinner('Finished Reading config.json file').succeed()
-
-// TODO---- start ---- until telegram page scraper is implemented
-configs = configs.filter(config => !config.type)
-// ---- end ----
-;(async () => {
-	// read the configration of each news and scrape
-	for (let { name, location } of configs) {
-		let config = readJSONfile(location)
-		await scrape(config)
-	}
-})()
-
+// scrape one news site
 async function scrape(config) {
 	try {
-		spinner(`Started Scrapping ${config.name} site`).start()
+		debugScraper(`Started Scrapping ${config.name} site`)
 		// getting all the news-link from the showcase
-		const links = await scrapeShowCase(config)
+		const urls = await scrapeShowCase(config)
 
 		// fetching all the news from each link in the links
-		let allNEWS = []
-		for (let link of links) {
-			let news = await scrapeNEWS(link, config)
-			allNEWS.push({ link, ...news })
+		for (let url of urls) {
+			let news = await scrapeNEWS(url, config)
+			saveNEWS({ url, ...news })
 		}
-
-		//saving all the news
-		saveNEWS(allNEWS, config.news.savingLocation)
-		spinner(`Finished Scrapping ${config.name} site`).succeed()
+		debugScraper(`Finished Scrapping ${config.name} site`)
 	} catch (err) {
-		spinner(`Failed Scrapping ${config.name}: ${err.message}`)
+		debugScraper(`Failed Scrapping ${config.name}: ${err.message}`)
 	}
 }
 // gets all the news-link from the showcase
@@ -57,10 +40,14 @@ async function scrapeNEWS(link, config) {
 	} catch (error) {}
 }
 // save the news to a file
-function saveNEWS(news, location) {
-	spinner('	Writting to a file Started').start()
-	fs.writeFileSync(location, JSON.stringify(news))
-	spinner('	Finished writting to a file').succeed()
+async function saveNEWS(news) {
+	debugScraper('	Writting to a file Started')
+	try {
+		await createNews(news)
+		debugScraper('	Finished writting to a file')
+	} catch (error) {
+		debugScraper('	Failed to write a file ' + error.message)
+	}
 }
 
 //read a json file and return the parsed json object
@@ -70,4 +57,27 @@ function readJSONfile(location) {
 	})
 	file = JSON.parse(file)
 	return file
+}
+
+async function runScraper() {
+	// reading the base configration file which contain path and to the news configration file
+	debugScraper('Started Reading config.json file')
+	let configs = readJSONfile(path.join(__dirname, 'configs', 'configs.json'))
+	debugScraper('Finished Reading config.json file')
+
+	// TODO---- start ---- until telegram page scraper is implemented
+	configs = configs.filter(config => !config.type)
+	// ---- end ----
+
+	const newsPromise = configs.map(({ name, location }) => {
+		let config = readJSONfile(location)
+		return scrape(config)
+	})
+
+	Promise.all(newsPromise)
+}
+
+module.exports = {
+	runScraper,
+	test,
 }
